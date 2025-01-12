dff8241415c5d865a05611050b681f4ce7de724a
ignacio <ignaciovegas7@gmail.com>
2025-01-12 00:29:17 -0300

    'prueba1'

--- src/main/java/com/example/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/errors/GlobalExceptionHandler.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//@RestControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
